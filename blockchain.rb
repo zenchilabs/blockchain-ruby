7153717465b2ae7374e1d9a1c569d6d3c1fe7bc7
James Larisch <root@jameslarisch.com>
2014-06-13 01:21:54 -0400

    rework some module placement, add address class

--- blockchain.rb
+++ blockchain.rb
@@ -1,7 +1,24 @@
 module Blockchain
+
   ROOT = 'https://blockchain.info'
+
+  def get(path)
+    response = open("#{ROOT}/#{path}")
+    response.read if response.status.first == '200'
+  end
+
+  module Query
+    QUERY = 'q'
+
+    def q(path)
+      get("#{QUERY}/#{path}")
+    end
+
+  end
 end
 
 require 'bigdecimal'
 require 'open-uri'
-require 'q'
+require 'date'
+require 'realtime'
+require 'address'
